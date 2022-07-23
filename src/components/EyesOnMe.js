61a3a3b8850868727d2bfeca4a118c56633a0085
Seth David Gyimah <esec26@gmail.com>
2022-07-23 20:53:27 +0000

    event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+
+function EyesOnMe(){
+
+    //function to handle onFocus
+    function focus(){
+        console.log("Good!")
+    }
+
+    //function to handle onBlur
+    function blur(){
+        console.log("Hey! Eyes on me!")
+    }
+
+    return <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,5 @@
-// Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad(){
+    return <input onChange={()=>console.log("Entering password...")} type="password"></input>
 }
 
-export default Keypad;
+export default Keypad
